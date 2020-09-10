0121a164578ad709120c0aeef4520cf1238cca00
shubh1646 <sharma.shubh@northeastern.edu>
2020-09-09 20:37:30 -0400

    Updates

---
+++ precache-manifest.705cabd8d9f07427b0a7bb3448ecc616.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "273f77d7c9b6fa6d8f14d3ae6d54b2b6",
+    "url": "/React-GitFrame/index.html"
+  },
+  {
+    "revision": "31244c21150f353187c9",
+    "url": "/React-GitFrame/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "10aa3ce8b5aaa87387a3",
+    "url": "/React-GitFrame/static/css/main.0d3540fe.chunk.css"
+  },
+  {
+    "revision": "31244c21150f353187c9",
+    "url": "/React-GitFrame/static/js/2.20bae3fe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-GitFrame/static/js/2.20bae3fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10aa3ce8b5aaa87387a3",
+    "url": "/React-GitFrame/static/js/main.0cd1031f.chunk.js"
+  },
+  {
+    "revision": "bdef08e782fb5b14d93c",
+    "url": "/React-GitFrame/static/js/runtime-main.b4c079f7.js"
+  }
+]);
